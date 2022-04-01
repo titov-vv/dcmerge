3bce1c35d6eba81cb71743828d272ef442a20885
Vlad <55182132+titov-vv@users.noreply.github.com>
2022-04-01 15:55:45 +0300

    DlsgFile.split_records() was implemented

--- dlsg_file.py
+++ dlsg_file.py
@@ -1,10 +1,13 @@
 import logging
 
 class DlsgFile:
+    LENGTH_SIZE = 4
     HEADER2021 = "DLSG            Decl20210103FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+    FOOTER = '\0'
 
     # Loads data from given file into internal class structures
     def __init__(self, filename):
+        self._records = []
         self._filename = filename
         logging.debug(f"Loading file: {filename}")
 
@@ -13,6 +16,7 @@ class DlsgFile:
         self.header = raw_data[:len(self.HEADER2021)]
         if self.header != self.HEADER2021:
             raise ValueError("Unsupported format of declaration file")
+        self._split_records(raw_data)
 
     # Appends data from another DlsgFile object referred by dlsg parameter
     def append(self, dlsg):
@@ -21,3 +25,21 @@ class DlsgFile:
     # Saves current data into initial file
     def save(self):
         pass
+
+    # this method splits declaration data into records stored in self._records
+    def _split_records(self, data):
+        pos = len(self.HEADER2021)
+        while pos < len(data):
+            length_field = data[pos: pos + self.LENGTH_SIZE]
+            if length_field == (self.FOOTER * len(length_field)):
+                self._footer_len = len(length_field)
+                break
+            try:
+                length = int(length_field)
+            except Exception as e:
+                logging.fatal(f"Invalid record size at position {pos}: '{length_field}'")
+                raise e
+            pos += self.LENGTH_SIZE
+            self._records.append(data[pos: pos + length])
+            pos = pos + length
+        logging.debug(f"Declaration {self._filename} content: {self._records}")
