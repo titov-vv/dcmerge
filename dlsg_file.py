a39b0e9e4072199ba347691d92f2dcc2646e3313
Vlad <55182132+titov-vv@users.noreply.github.com>
2022-04-06 11:25:22 +0300

    Handle the case when "base" file has zero child records in section to copy (@DeclForeign was present but had no @CurrencyIncomeXXXX).

--- dlsg_file.py
+++ dlsg_file.py
@@ -33,8 +33,12 @@ class DlsgFile:
         for section in self.merge_list:
             dst = self.get_section(section)
             dst_size = dst.get_child_count()
-            last_child = self.get_section(self.merge_list[section] + f"{(dst_size - 1):04d}")
-            last_idx = self._sections.index(last_child)
+            if dst_size:
+                section_to_continue = self.get_section(self.merge_list[section] + f"{(dst_size - 1):04d}")
+            else:
+                logging.debug("No foreign incomes in source file")
+                section_to_continue = self.get_section(section)
+            last_idx = self._sections.index(section_to_continue)
             src = dlsg.get_section(section)
             src_size = src.get_child_count()
             for i in range(src_size):
--- tests/test_dcmerge.py
+++ tests/test_dcmerge.py
@@ -7,7 +7,7 @@ from dcmerge import main
 def test_dcmerge(monkeypatch, tmp_path):
     output = str(tmp_path) + os.sep + "output.dc1"
     with monkeypatch.context() as m:
-        m.setattr(sys, 'argv', ['dcmerge', '--base', './base.dc1', '--add', './delta.dc1', '--out', output])
+        m.setattr(sys, 'argv', ['dcmerge', '--base', './ilya2021.dc1', '--add', './my.dc1', '--out', output])
         assert main() is None
         assert filecmp.cmp('./result.dc1', output)
         os.remove(output)
